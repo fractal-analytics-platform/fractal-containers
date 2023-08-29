4890270fb1b2bb03edf208460308567b306df28a
aangelone2 <adriano.angelone@exact-lab.it>
2023-08-29 14:10:40 +0200

    installed precommit hooks

--- gen_requirements.py
+++ gen_requirements.py
@@ -1,3 +1,10 @@
+"""Generates requirements.txt file by querying PyPI.
+
+Usage:
+    python gen_requirements <package_name> <destination file>
+"""
+
+
 import sys
 import urllib.request
 import json
@@ -12,11 +19,13 @@ print(f"Querying {url}...")
 with urllib.request.urlopen(url) as site:
     res = json.load(site)["info"]["requires_dist"]
 
-with open(outfile, "w") as out:
+with open(outfile, "w", encoding="utf-8") as out:
     for package in res:
         cleaned = re.sub(" ;.*", "", package)
 
         # adding fix for psycopg2
-        cleaned = re.sub("psycopg2 ", "psycopg2-binary ", cleaned)
+        cleaned = re.sub(
+            "psycopg2 ", "psycopg2-binary ", cleaned
+        )
 
         print(cleaned, file=out)
