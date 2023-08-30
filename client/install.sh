e51a7b383125e25329f73050b69be48ae39513a3
aangelone2 <adriano.angelone@exact-lab.it>
2023-08-30 08:36:02 +0200

    separated client and server files, decomposed client script CLIENT STILL NOT WORKING

---
+++ client/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RELEASE="0.5.2"
+
+echo "*** Fetching a clean copy of fractal-web ${RELEASE} ***"
+
+rm -fr fractal-web-${RELEASE}
+rm -f ${RELEASE}.tar.gz
+
+wget https://github.com/fractal-analytics-platform/fractal-web/archive/refs/tags/${RELEASE}.tar.gz
+tar -xvf ${RELEASE}.tar.gz
+
+mv fractal-web-${RELEASE}/ fractal-web/
+rm ${RELEASE}.tar.gz
