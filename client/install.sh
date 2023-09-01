01ff4f1d07a07dbef3cd58f7d5e0b945c931ce34
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-09-01 15:05:46 +0200

    Remove nginx proxy, and switch to `274-fix-cookie-issue` fractal-web branch

--- client/install.sh
+++ client/install.sh
@@ -7,8 +7,12 @@ echo "*** Fetching a clean copy of fractal-web ${RELEASE} ***"
 rm -fr fractal-web-${RELEASE}
 rm -f ${RELEASE}.tar.gz
 
-wget https://github.com/fractal-analytics-platform/fractal-web/archive/refs/tags/${RELEASE}.tar.gz
-tar -xvf ${RELEASE}.tar.gz
+# wget https://github.com/fractal-analytics-platform/fractal-web/archive/refs/tags/${RELEASE}.tar.gz
+# tar -xvf ${RELEASE}.tar.gz
+# mv fractal-web-${RELEASE}/ fractal-web/
+# rm ${RELEASE}.tar.gz
 
-mv fractal-web-${RELEASE}/ fractal-web/
-rm ${RELEASE}.tar.gz
+git clone https://github.com/fractal-analytics-platform/fractal-web.git
+cd fractal-web
+git checkout 274-fix-cookie-issue
+
