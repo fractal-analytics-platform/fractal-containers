5218510a8498fd9ded8f228252a117842f156b0c
aangelone2 <adriano.angelone@exact-lab.it>
2023-09-04 11:15:39 +0200

    moved slurm configuration to slurm/ folder simplified root docker-compose

---
+++ slurm/client/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#RELEASE="0.5.3"
+
+#echo "*** Fetching a clean copy of fractal-web ${RELEASE} ***"
+
+#rm -fr fractal-web-${RELEASE}
+#rm -f ${RELEASE}.tar.gz
+
+# wget https://github.com/fractal-analytics-platform/fractal-web/archive/refs/tags/${RELEASE}.tar.gz
+# tar -xvf ${RELEASE}.tar.gz
+# mv fractal-web-${RELEASE}/ fractal-web/
+# rm ${RELEASE}.tar.gz
+
+rm -rf fractal-web/
+
+git clone https://github.com/fractal-analytics-platform/fractal-web.git
+cd fractal-web
+git checkout 274-fix-cookie-issue
