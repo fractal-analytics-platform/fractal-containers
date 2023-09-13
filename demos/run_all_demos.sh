6af8eceb3956986bea679ce1f2a5deb1d6600715
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-09-13 16:35:54 +0200

    Include task collection in fractal-demos

--- demos/run_all_demos.sh
+++ demos/run_all_demos.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 
-# I AM IN /home/fractal-share
+cp -v .fractal.env /home/fractal-share/
+cd /home/fractal-share
+pwd
 
 git clone https://github.com/fractal-analytics-platform/fractal-demos.git
 
 cd fractal-demos/examples
-cp ../../.fractal.env .
+pwd
+cp -v ../../.fractal.env .
+
+# Trigger task collection
+fractal task collect fractal-tasks-core --package-version 0.11.0 --package-extras fractal-tasks
+
+# Wait for the end of task collection
+while [ "$(fractal task list)" == "[]" ]; do
+    echo "No task available, wait 10 seconds.";
+    sleep 10;
+done
+fractal task list
 
 # Run example 01
 echo "RUN EXAMPLE 01 - START"
