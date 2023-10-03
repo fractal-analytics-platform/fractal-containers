b17e255dcf1fa8842b851fde84fc2c6f5f7aaaa6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-03 16:31:21 +0200

    Update handling of versions

--- server/install_fractal_server.sh
+++ server/install_fractal_server.sh
@@ -1,3 +1,6 @@
+export $(grep -v '^#' config_env.sh | xargs)
+export FRACTAL_SERVER_RELEASE=asdasd
+
 echo "FRACTAL_SERVER_RELEASE: $FRACTAL_SERVER_RELEASE"
 echo "FRACTAL_SERVER_GIT: $FRACTAL_SERVER_GIT"
 
