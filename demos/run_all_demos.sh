a9a3a5ca5f34d09fe1af994c2cc64e2c4a58d8e2
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-03 15:17:14 +0200

    Multiple updates related to #18

--- demos/run_all_demos.sh
+++ demos/run_all_demos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Wait for the end of task collection
+cd /home/fractal_share/fractal-demos/examples
+
+cp .fractal.env 00_user_setup
+
+# TASK COLLECTION
+fractal task collect fractal-tasks-core --package-version 0.12.0 --package-extras fractal-tasks
 while [ "$(fractal task list)" == "[]" ]; do
     echo "No task available, wait 10 seconds.";
     sleep 10;
@@ -8,30 +13,49 @@ done
 fractal task list
 
 # Run example 01
-echo "RUN EXAMPLE 01 - START"
 cd 01_cardio_tiny_dataset
 mkdir -p ../images
 cp ../.fractal.env .
+
+# FIXME: replace with cp -r /home/fractal_share/resources/...
 ./fetch_test_data_from_zenodo.sh
 
-./run_example.sh
+LOGFILE="log_01.txt"
 
-# FIXME: run_example.sh writes a lot of lines, the last one being the job ID. This must be extracted
+echo "RUN EXAMPLE 01 - START" >> $LOGFILE
+./run_example.sh >> $LOGFILE 2>&1
+API_EXITCODE=$?
+cat $LOGFILE
+PROJECT_ID=$(cat $LOGFILE | grep PRJ_ID | cut -d ':' -f 2)
+JOB_ID=$(tail -n 1 $LOGFILE)
+echo "PROJECT_ID: $PROJECT_ID"
 
-# FIXME: Given the job ID, we need to wait until the job is complete/failed.
+while true; do
+    STATUS_LINE=$(fractal job show $PROJECT_ID $JOB_ID | grep "status")
+    echo $STATUS_LINE
+    if [[ "$STATUS_LINE" == *done* || "$STATUS_LINE" == *failed* ]]; then
+        break
+    fi
+    sleep 1
+done
 
-# FIXME: we need to get the exit code of run_example.sh, and to log it.
+# Write status to log
+fractal job show $PROJECT_ID $JOB_ID
 
-# FIXME: run validation script, and log its output/exitcode. Something like this.
-# This should be something like
-# $ LOGFILE="log.txt"
-# $ SUMMARY="summary.txt"
-# $ echo "START RESULTS VALIDATION" >> $LOGFILE
-# $ python validate_results.py >> $LOGFILE 2>&1
-# $ VALIDATION_EXIT_CODE=$?
-# $ echo "END RESULTS VALIDATION" >> $LOGFILE
-# $ echo "Validation exit code: $VALIDATION_EXIT_CODE" >> $LOGFILE
-# $ echo "Validation exit code: $VALIDATION_EXIT_CODE" >> $SUMMARY
+echo "START RESULTS VALIDATION"
+echo
+pwd
+ls -lh
+echo
+python validate_results.py >> tmp 2>&1
+VALIDATION_EXIT_CODE=$?
+echo
+cat tmp
+echo
+echo "END RESULTS VALIDATION"
+echo
+echo "Validation exit code: $VALIDATION_EXIT_CODE"
+echo
 
 
 cd ..
--- server/install_fractal_server.sh
+++ server/install_fractal_server.sh
@@ -1,3 +1,5 @@
-echo "FRACTAL_VERSION = $FRACTAL_VERSION"
+echo "FRACTAL_SERVER_RELEASE: $FRACTAL_SERVER_RELEASE"
+echo "FRACTAL_SERVER_GIT: $FRACTAL_SERVER_GIT"
 
 pip install fractal-server[postgres,gunicorn]==1.3.8a3
+pip install fractal-client==1.3.6
