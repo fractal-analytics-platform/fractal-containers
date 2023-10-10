8d891e87582e6aaa3f04f8d92a79895ff8ce5e80
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-10 11:12:47 +0200

    build fractal_tasks_core

--- demos/get_fractal_tasks_core.sh
+++ demos/get_fractal_tasks_core.sh
@@ -1,3 +1,11 @@
+check_exit_code() {
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "Error: EXIT_CODE=$EXIT_CODE"
+        exit 1
+    fi
+}
+
 source config.env
 echo "FRACTAL_TASKS_CORE_RELEASE=$FRACTAL_TASKS_CORE_RELEASE"
 echo "FRACTAL_TASKS_CORE_GIT=$FRACTAL_TASKS_CORE_GIT"
@@ -7,8 +15,27 @@ if [ -z "${FRACTAL_TASKS_CORE_RELEASE}" ]; then
         fractal task collect fractal-tasks-core --package-extras fractal-tasks
     else
         # Case 2: only git set
-        # TODO: git clone, git checkout, install poetry, poetry build, collect from local wheel file
-        echo "Error: FRACTAL_TASKS_CORE_GIT not supported" && exit 1
+        git clone https://github.com/fractal-analytics-platform/fractal-tasks-core.git
+        cd fractal-tasks-core
+        git checkout FRACTAL_TASKS_CORE_GIT
+        check_exit_code
+
+        curl -sSL https://install.python-poetry.org | python3 -
+        which poetry
+        check_exit_code
+
+        poetry build
+        WHL=`ls dist/*.whl`
+        check_exit_code
+
+        if [[ $WHL == /* ]]; then
+            ABS_WHL="$WHL"
+        else
+            ABS_WHL="$(pwd)/$WHL"
+        fi
+
+        cd ..
+        fractal task collect $ABS_WHL --package-extras fractal-tasks
     fi
 else
     if [ -z "${FRACTAL_TASKS_CORE_GIT}" ]; then
