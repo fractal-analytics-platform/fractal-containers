0cdfc36fdf635ec589a1edb483367a4ea1f0a5ac
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-10 10:44:55 +0200

    clone fractal-demos

--- demos/get_fractal_demos.sh
+++ demos/get_fractal_demos.sh
@@ -1,16 +1,17 @@
+git clone https://github.com/fractal-analytics-platform/fractal-demos.git
+
 source config.env
 
+echo "FRACTAL_DEMOS_GIT=$FRACTAL_DEMOS_GIT"
 if [ ! -z "${FRACTAL_DEMOS_GIT}" ]; then
-    echo "FRACTAL_DEMOS_GIT=$FRACTAL_DEMOS_GIT"
-    python3 -m pip install git+https://github.com/fractal-analytics-platform/fractal-demos.git@$FRACTAL_DEMOS_GIT
-else
-echo "FRACTAL_DEMOS_GIT=main"
-    python3 -m pip install git+https://github.com/fractal-analytics-platform/fractal-demos.git
-fi
-
-# Check exit code
-PIP_EXIT_CODE=$?
-if [ $PIP_EXIT_CODE -ne 0 ]; then
-    echo "Error: PIP_EXIT_CODE=$PIP_EXIT_CODE"
-    exit 1
+    cd fractal-demos
+    git checkout $FRACTAL_DEMOS_GIT
+    
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        echo "Error: EXIT_CODE=$EXIT_CODE"
+        exit 1
+    fi
+    
+    cd ..
 fi
