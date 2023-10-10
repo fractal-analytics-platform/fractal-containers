8d31b217b29a5d9aed930ff13a8f222fe7fd62dc
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-10 10:17:43 +0200

    handle github installation

--- demos/get_fractal_demos.sh
+++ demos/get_fractal_demos.sh
@@ -1,10 +1,16 @@
-git clone https://github.com/fractal-analytics-platform/fractal-demos.git
-
 source config.env
 
-echo "FRACTAL_DEMOS_GIT=$FRACTAL_DEMOS_GIT"
 if [ ! -z "${FRACTAL_DEMOS_GIT}" ]; then
-    cd fractal-demos
-    git checkout $FRACTAL_DEMOS_GIT
-    cd ..
+    echo "FRACTAL_DEMOS_GIT=$FRACTAL_DEMOS_GIT"
+    python3 -m pip install git+https://github.com/fractal-analytics-platform/fractal-demos.git@$FRACTAL_DEMOS_GIT
+else
+echo "FRACTAL_DEMOS_GIT=main"
+    python3 -m pip install git+https://github.com/fractal-analytics-platform/fractal-demos.git
+fi
+
+# Check exit code
+PIP_EXIT_CODE=$?
+if [ $PIP_EXIT_CODE -ne 0 ]; then
+    echo "Error: PIP_EXIT_CODE=$PIP_EXIT_CODE"
+    exit 1
 fi
