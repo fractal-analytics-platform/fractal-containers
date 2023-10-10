1ad5878984b85bbde1d1f150258d3d047188b7ca
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-10 11:19:39 +0200

    fix typo and rm check

--- demos/get_fractal_tasks_core.sh
+++ demos/get_fractal_tasks_core.sh
@@ -17,7 +17,7 @@ if [ -z "${FRACTAL_TASKS_CORE_RELEASE}" ]; then
         # Case 2: only git set
         git clone https://github.com/fractal-analytics-platform/fractal-tasks-core.git
         cd fractal-tasks-core
-        git checkout FRACTAL_TASKS_CORE_GIT
+        git checkout $FRACTAL_TASKS_CORE_GIT
         check_exit_code
 
         curl -sSL https://install.python-poetry.org | python3 -
@@ -27,13 +27,8 @@ if [ -z "${FRACTAL_TASKS_CORE_RELEASE}" ]; then
         poetry build
         WHL=`ls dist/*.whl`
         check_exit_code
-
-        if [[ $WHL == /* ]]; then
-            ABS_WHL="$WHL"
-        else
-            ABS_WHL="$(pwd)/$WHL"
-        fi
-
+        ABS_WHL="$(pwd)/$WHL"
+        
         cd ..
         fractal task collect $ABS_WHL --package-extras fractal-tasks
     fi
