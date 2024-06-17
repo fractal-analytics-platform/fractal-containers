d58857074e14d55ff1983303b49d9eec83e04090
mfranzon <franzonmarco94@gmail.com>
2024-06-17 12:28:43 +0200

    [skip ci] configure slurm with just one node, remove useless scritps

--- slurm-stress/fractal-slurm/docker-entrypoint.sh
+++ slurm-stress/fractal-slurm/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-: "${SLURM_CONF_IN=/etc/slurm/slurm.conf.in}"
 : "${SLURM_CONF=/etc/slurm/slurm.conf}"
 
 # Default number of slurm nodes
@@ -18,46 +17,13 @@
 # Default hardware profile
 : "${NODE_HW=CPUs=4}"
 
-#sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm/slurm.conf
 
-# Generate node names and associated ports
-NODE_NAMES=$(printf "nd[%05i-%05i]" 1 $SLURM_NUMNODES)
-NODE_PORTS=$(printf "%i-%i" $NODE_BASEPORT $(($NODE_BASEPORT+$SLURM_NUMNODES-1)))
-
-
-echo "INFO:"
-echo "INFO: Creating $SLURM_CONF with"
-echo "INFO: "
-column -t <<-EOF
-      INFO: SLURMCTLD_HOST=$SLURMCTLD_HOST SLURMCTLD_ADDR=$SLURMCTLD_ADDR
-      INFO: NODE_HOST=$NODE_HOST NODE_ADDR=$NODE_ADDR NODE_BASEPORT=$NODE_BASEPORT
-      INFO: NODE_HW=$NODE_HW
-      INFO: SLURM_NUMNODES=$SLURM_NUMNODES
-EOF
-echo "INFO: "
-echo "INFO: Derived values:"
-echo "INFO:"
-column -t <<-EOF
-      INFO: NODE_NAMES=$NODE_NAMES
-      INFO: NODE_PORTS=$NODE_PORTS
-EOF
-echo "INFO:"
-echo "INFO: Override any of the non-derived values by setting the respective environment variable"
-echo "INFO: when starting Docker."
-echo "INFO:"
+NODE_NAMES="nd00001"
+NODE_PORTS="6001"
 
 export PATH=$SLURM_ROOT/bin:$PATH
 export LD_LIBRARY_PATH=$SLURM_ROOT/lib:$LD_LIBRARY_PATH
 export MANPATH=$SLURM_ROOT/man:$MANPATH
-
-(
-    echo "NodeName=${NODE_NAMES} NodeHostname=${NODE_HOST} NodeAddr=${NODE_ADDR} Port=${NODE_PORTS} State=UNKNOWN ${NODE_HW}"
-    echo "PartitionName=dkr Nodes=ALL Default=YES MaxTime=INFINITE State=UP"
-) \
-| sed -e "s/SLURMCTLDHOST/${SLURMCTLD_HOST}/" \
-      -e "s/SLURMCTLDADDR/${SLURMCTLD_ADDR}/" \
-    $SLURM_CONF_IN - \
-> $SLURM_CONF
 
 NODE_NAME_LIST=$(scontrol show hostnames $NODE_NAMES)
 
@@ -71,11 +37,7 @@ echo "Starting Slurm services..."
 echo
 
 $SLURM_ROOT/sbin/slurmctld
-
-for n in $NODE_NAME_LIST
-do
-    $SLURM_ROOT/sbin/slurmd -N $n
-done
+$SLURM_ROOT/sbin/slurmd
 
 echo
 sinfo
