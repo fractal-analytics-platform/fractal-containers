63412e1a7c93e09231fdd94aecdc880406958756
Marco Franzon <43796979+mfranzon@users.noreply.github.com>
2024-09-11 12:19:59 +0200

    [skip ci] make chmod recursive

--- examples/full-stack/filebrowser/check_file.sh
+++ examples/full-stack/filebrowser/check_file.sh
@@ -3,6 +3,6 @@ DIRECTORY_TO_WATCH="/data"
 echo $$ > /run/monitord.pid
 # Monitor the directory and change permissions
 inotifywait -m -e create --format '%w%f' "$DIRECTORY_TO_WATCH" | while read NEW_FILE; do
-  chmod 0666 "$NEW_FILE"
+  chmod -R 0666 "$NEW_FILE"
   echo "Changed permissions for $NEW_FILE"
 done
