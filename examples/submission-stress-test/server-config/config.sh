1117c28f4eb265ba34b58f2e70cb635f0614b4f3
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-01-28 15:23:59 +0100

    update submission-stress-test example and add one more makefile

--- examples/submission-stress-test/server-config/config.sh
+++ examples/submission-stress-test/server-config/config.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Collect tasks - not required for demo purposes but could be useful having the command here
-fractal task collect /fractal_tasks_mock-0.0.1-py3-none-any.whl
-
 # Update user
 FRACTAL_USER_ID=$(fractal --batch user whoami)
 fractal user edit "$FRACTAL_USER_ID" --new-project-dir /home/test01 --new-slurm-user test01
+
+# Collect tasks - not required for demo purposes but could be useful having the command here
+wget https://github.com/fractal-analytics-platform/fractal-server/raw/refs/heads/main/tests/v2/fractal_tasks_mock/dist/fractal_tasks_mock-0.0.1-py3-none-any.whl
+fractal task collect ./fractal_tasks_mock-0.0.1-py3-none-any.whl
 
 # Create folders and change permissions
 mkdir -p /data/images/
