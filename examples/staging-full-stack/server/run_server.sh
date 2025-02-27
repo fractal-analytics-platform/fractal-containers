614d9ea27adb2bac433ee609c23ffafceea39cc0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-02-27 17:31:27 +0100

    staging-full-stack

---
+++ examples/staging-full-stack/server/run_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+fractalctl set-db
+
+gunicorn fractal_server.main:app \
+    --workers 2 \
+    --timeout 20 \
+    --bind 0.0.0.0:8000 \
+    --access-logfile - \
+    --error-logfile - \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --logger-class fractal_server.gunicorn_fractal.FractalGunicornLogger
