850f7562a0042865f6aa0d4fb7a132f689ff41bb
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2025-04-29 15:07:06 +0200

    keep improving ssh-based example

--- examples/ssh-based/slurm_cluster/get_fractal_server.sh
+++ examples/ssh-based/slurm_cluster/get_fractal_server.sh
@@ -5,10 +5,10 @@ echo "FRACTAL_SERVER_GIT=$FRACTAL_SERVER_GIT"
 if [ -z "${FRACTAL_SERVER_RELEASE}" ]; then
     if [ -z "${FRACTAL_SERVER_GIT}" ]; then
         # Case 1: no release, no git
-        python3 -m pip install "fractal-server"
+        /fractal-server-venv/bin/python3 -m pip install "fractal-server"
     else
         # Case 2: only git set
-        python3 -m pip install "fractal-server @ git+https://github.com/fractal-analytics-platform/fractal-server.git@${FRACTAL_SERVER_GIT}"
+        /fractal-server-venv/bin/python3 -m pip install "fractal-server @ git+https://github.com/fractal-analytics-platform/fractal-server.git@${FRACTAL_SERVER_GIT}"
         EXIT_CODE=$?
         if [ $EXIT_CODE -ne 0 ]; then
             echo "Error: Installing fractal-server@$FRACTAL_SERVER_GIT (EXIT_CODE=$EXIT_CODE)"
@@ -18,7 +18,7 @@ if [ -z "${FRACTAL_SERVER_RELEASE}" ]; then
 else
     if [ -z "${FRACTAL_SERVER_GIT}" ]; then
         # Case 3: only release set
-        python3 -m pip install "fractal-server==$FRACTAL_SERVER_RELEASE"
+        /fractal-server-venv/bin/python3 -m pip install "fractal-server==$FRACTAL_SERVER_RELEASE"
     else
         # Case 4: both release and git set
         echo "Error: cannot set both FRACTAL_SERVER_RELEASE and FRACTAL_SERVER_GIT." && exit 1
