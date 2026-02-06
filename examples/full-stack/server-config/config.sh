7588936571aed191ac1dc5591627b582a5dd0953
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2026-02-06 09:40:49 +0100

    Create folders before zenodo downloads, which could take long

--- examples/full-stack/server-config/config.sh
+++ examples/full-stack/server-config/config.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 set -eu
+set -o xtrace
 
 # shellcheck disable=SC1091
 source ./venv-client/bin/activate
 
-# Download test zarr data
+# Create folders and make them 777
+# NOTE: Do this before accessing zenodo data, because that operation could hit some rate limit and stall
+mkdir -p /data/images
 mkdir -p /data/zarrs
+chmod 777 /data/images/ --recursive
+chmod 777 /data/zarrs/ --recursive
+
+# Download zarr data
 # shellcheck disable=SC2164
 cd /data/zarrs/
 wget --quiet https://zenodo.org/records/10424292/files/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr.zip
@@ -14,7 +21,6 @@ unzip -q 20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr.zip
 rm -r 20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr.zip __MACOSX
 
 # Download test image data
-mkdir -p /data/images
 mkdir /data/images/10.5281_zenodo.8287221
 # shellcheck disable=SC2164
 cd /data/images/10.5281_zenodo.8287221
@@ -23,6 +29,6 @@ mv files-archive files-archive.zip
 unzip -q files-archive.zip
 rm files-archive.zip
 
-# Change permissions
+# Re-apply broad permissions
 chmod 777 /data/images/ --recursive
 chmod 777 /data/zarrs/ --recursive
