a7dbb1e415f9d6999c050005282ff02f0db74edf
Sonia Zorba <sonia.zorba@mailbox.org>
2026-07-17 15:26:45 +0200

    Improve starting of slurmctld

--- images/ubuntu24-slurm/docker-entrypoint.sh
+++ images/ubuntu24-slurm/docker-entrypoint.sh
@@ -22,9 +22,27 @@ echo
 service mariadb start
 service munge start
 service slurmdbd start
+
+# slurmctld
+attempt=1
+max_seconds=10
 service slurmctld start
-service slurmctld stop
-service slurmctld start
+while [ "$attempt" -le $max_seconds ]; do
+  status=$(service slurmctld status 2>&1)
+  if [ -z "$status" ]; then
+    echo "[W] Empty response from \"slurmctld status\": trying to start slurmctld again..."
+    service slurmctld start
+  elif service slurmctld status 2>&1 | grep -q "running"; then
+    break
+  fi
+  sleep 1
+  attempt=$((attempt + 1))
+done
+if [ $attempt -ge $max_seconds ]; then
+  echo "slurmctld didn't reach running status after $max_seconds seconds" >&2
+  exit 1
+fi
+
 service slurmd start
 slurmd -N $NODE
 
