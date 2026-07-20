294aca5786156918047ffb852def9665cac0c424
Sonia Zorba <sonia.zorba@mailbox.org>
2026-07-20 10:31:23 +0200

    Improve starting of slurmctld by waiting for port 6819

--- images/ubuntu24-slurm/docker-entrypoint.sh
+++ images/ubuntu24-slurm/docker-entrypoint.sh
@@ -23,25 +23,12 @@ service mariadb start
 service munge start
 service slurmdbd start
 
-# slurmctld
-attempt=1
-max_seconds=10
-service slurmctld start
-while [ "$attempt" -le $max_seconds ]; do
-  status=$(service slurmctld status 2>&1)
-  if [ -z "$status" ]; then
-    echo "[W] Empty response from \"slurmctld status\": trying to start slurmctld again..."
-    service slurmctld start
-  elif service slurmctld status 2>&1 | grep -q "running"; then
-    break
-  fi
+while ! bash -c "</dev/tcp/127.0.0.1/6819" 2>/dev/null; do
+  echo "Waiting for port 6819 to be ready..."
   sleep 1
-  attempt=$((attempt + 1))
 done
-if [ $attempt -ge $max_seconds ]; then
-  echo "slurmctld didn't reach running status after $max_seconds seconds" >&2
-  exit 1
-fi
+
+service slurmctld start
 
 service slurmd start
 slurmd -N $NODE
